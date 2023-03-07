f8c581f1969b24ef95c8e5ab35e871ace3a4a38d
rooru9 <109156411+ZoeyNjoy@users.noreply.github.com>
2023-03-07 12:58:30 +0900

    230307

---
+++ gallery.js
@@ -0,0 +1,40 @@
+$(function(){
+  var $container = $('.g_list'),
+      $loadBtn= $('.load-more'),
+      $addItemCount = 6,
+      $added = 0, //더보기 버튼을 클릭해서 추가된 항목수가 0가 될때 버튼을 사라지게
+      $allData = [];
+  
+  $.getJSON('./content.json', initGallery)
+    function initGallery(data){
+      $allData = data;      
+      addItem();
+      $loadBtn.click(addItem);
+    }
+        /* 
+          $.getJSON('./content.json', initGallery)  =
+          $.getJSON('./content.json', functin( data ){
+            initGallery( data ); })라고 쓰는 것과 같다
+        */
+  
+  function addItem(){
+    var slicedData;
+    var elements = [];
+    /*  var A = [0,1,2,3,4]  A = allData
+        var b = A.slice(0,2)  A배열에서 0-2번째전까지의 값을 가져온다 b=slicedData
+    = 0,1을 가지고 온다는 뜻 */
+
+    slicedData = $allData.slice($added, $added+$addItemCount);
+    $.each(slicedData, function(idx, item){
+      var itemHTML =
+       '<li class="g_item"><a href="#"><p>'+ item.date +'<span>'+ item.no +'</span></p>' + 
+       '<img src="'+ item.images +'"alt="이미지"><h3>'+ item.title +'</h3><p class="item_cont">'+ item.contents +'</p><span class="plus">'+ item.more +'</span></a></li>';
+
+        elements.push($(itemHTML).get(0));
+        
+    });
+    $container.append(elements);
+    $added += slicedData.length;
+
+  }
+}); // ready function
